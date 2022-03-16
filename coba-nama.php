55961f1273314184dbcb6b96f3f28e9f23740245
tamami <tamami.oka@gmail.com>
2022-03-16 13:46:46 +0700

    add coba-nama file

---
+++ coba-nama.php
@@ -0,0 +1,3 @@
+<?php
+  echo "<h1>Nama saya Tamami</h1>";
+?>
--- connection.php
+++ connection.php
@@ -1,5 +1,6 @@
 <?php
-$conn_string = "host=172.17.0.2 port=5432 dbname=wp2_d user=postgres password=secret";
+$conn_string = "host=172.17.0.2 port=5432 dbname=wp2_d " .
+  "user=postgres password=secret";
 $conn = pg_connect($conn_string);
 
 if($conn) {
